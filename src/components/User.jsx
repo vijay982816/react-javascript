486d646f160873482f54ca4040e000d69513361e
vijay982816 <vijay982816vijay@gmail.com>
2022-09-16 13:28:25 +0000

    doing add user featcher

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,6 +1,8 @@
+
 import { useEffect } from "react"
 import { useContext } from "react"
 import UserContext from "../context/UserContext.jsx"
+import Modal from "./Modal.jsx"
 import SingleUser from "./SingleUser.jsx"
 
 const User = () => {
@@ -19,23 +21,10 @@ const User = () => {
     return (
         <>
 
-            {/* 
-            {users.map((element) =>
 
 
 
-
-                <P>{elment.name}</P>
-
-
-
-
-
-
-            )} */}
-
-
-            <div className="antialiased font-sans bg-gray-200">
+            {/* <div className="antialiased font-sans bg-gray-200">
                 <div className="container mx-auto px-4 sm:px-8">
                     <div className="py-8 lg:px-60">
                         <div>
@@ -67,48 +56,6 @@ const User = () => {
                                     </thead>
                                     <tbody>
 
-                                        {/* 
-
-                                        <tr>
-                                            <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                                <div className="flex items-center">
-                                                    <div className="flex-shrink-0 w-10 h-10">
-                                                        <img className="w-full h-full rounded-full"
-                                                            src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
-                                                            alt="" />
-                                                    </div>
-                                                    <div className="ml-3">
-                                                        <p className="text-gray-900 whitespace-no-wrap">
-                                                            Hitesh kumawat
-                                                        </p>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                                <p className="text-gray-900 whitespace-no-wrap">20</p>
-                                            </td>
-                                            <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                                <p className="text-gray-900 whitespace-no-wrap">
-                                                    8740096375
-                                                </p>
-                                            </td>
-                                            <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm text-center md:text-left">
-
-                                                <button className="my-1 px-3 py-1 font-semibold text-green-900 leading-tight bg-green-100 rounded-full mx-2">
-
-                                                    Update
-                                                </button>
-
-                                                <button className=" my-1 font-semibold text-red-900 leading-tight rounded-full bg-red-100 px-3 py-1">
-
-                                                    Delete
-                                                </button>
-
-                                            </td>
-                                        </tr> */}
-
-
-
 
                                         {users.map((element) => {
 
@@ -129,23 +76,24 @@ const User = () => {
 
                                 <div
                                     class="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between          ">
-                                 
+
                                     <div class="inline-flex mt-2 xs:mt-0">
                                         <button
                                             class="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-l">
                                             Add User
                                         </button>
-                                       
+
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> */}
 
 
 
+            <Modal />
 
 
 
--- src/context/AllUser.jsx
+++ src/context/AllUser.jsx
@@ -17,7 +17,7 @@ const AllUser = (props) => {
 
         const response = await fetch(`https://3001-vijay982816-blanknode-mtewnqs608r.ws-us65.gitpod.io/`);
 
-     
+
         // console.log(await response.json())
 
         const myjson = await response.json()
@@ -26,6 +26,28 @@ const AllUser = (props) => {
         setUsers(myjson)
 
     }
+
+
+
+
+    // Add a Note
+    const addNote = async (name, age, phone) => {
+        // TODO: API Call
+        // API Call 
+        const response = await fetch(`${host}/api/notes/addnote`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjEzMWRjNWUzZTQwMzdjZDQ3MzRhMDY2In0sImlhdCI6MTYzMDY2OTU5Nn0.hJS0hx6I7ROugkqjL2CjrJuefA3pJi-IU5yGUbRHI4Q"
+            },
+            body: JSON.stringify({ title, description, tag })
+        });
+
+        const note = await response.json();
+        setNotes(notes.concat(note))
+    }
+
+
 
 
 
